89e44b54092e31cf55f8f820154368234f94f5a4
Shaun <nabong.seanandreiluis@student.auf.edu.ph>
2025-11-13 22:45:28 +0800

    Sean Nabong

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
+    alias(libs.plugins.realm.android)
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.realm.android)
 }
 
 android {
